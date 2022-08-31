81ef079ead1ce3dce5af5ed36b47407e3e36cbbf
vladbuk <vladbuk@gmail.com>
2022-08-31 13:52:29 +0300

    quoted and unquoted positional args

--- args.sh
+++ args.sh
@@ -1,9 +1,34 @@
 #!/bin/bash
 
-#echo $#
-#echo $@
+echo "Numbers of args: $#"
+echo 'Args in array: $@'
+for i in $@
+do
+  echo $i
+done
+
+echo 'Args in quoted array: "$@"'
+for i in "$@"
+do
+  echo $i
+done
+
+
+echo 'Args in string: $*'
+for i in $*
+do
+  echo $i
+done
+
+echo 'Args in quoted string: "$*"'
+for i in "$*"
+do
+  echo $i
+done
+
+
 #a=$@
-echo ${@: -1}
+#echo ${@: -1} # last arg
 #echo ${!#}
 #echo ${#}
 #echo ${@: -1}
