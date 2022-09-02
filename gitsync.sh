91442a178450cc24b7ee46eef96754ff0257387e
vladbuk <vladbuk@gmail.com>
2022-09-02 14:44:38 +0300

    refactoring gitsync.sh

--- gitsync.sh
+++ gitsync.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 
-set -ex
+set -e
+
+[[ $# -ne 1 ]] && echo -e "Use gitsync.sh \"commit message\"" && exit 1
 
 git pull
 git status
 git add .
-if [[ $1 -ne 1 ]]
-then
- echo "use only one agr - commit message"
- exit 1
-elif [[ -z "$#" ]]
-then
-  echo "add commit message as arg"
-  exit
-else
-  git commit -m "$1"
-fi
+git commit -m "$1"
 git push
