ca63b14a070fa2c9aea8b2e330848324f6cc5c5b
vladbuk <vladbuk@gmail.com>
2022-09-16 13:53:55 +0300

    continue

--- fortest/01_grep.sh
+++ fortest/01_grep.sh
@@ -2,8 +2,10 @@
 
 if [[ -n $1 && -n $2 ]]
 then
-    grep -F -v $1 $2 > ${2}.tmp && mv ${2}.tmp $2
-    cat $2
+    grep -F -v $1 $2 > ${2}.edited
+    cat $2 ${2}.edited
 else
     echo "Use first arg for word and second arg for filename"
 fi
+
+# You can also use: sed -i '/word/d' poem.txt
