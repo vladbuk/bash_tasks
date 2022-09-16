c894df7e54c2e106d64d3b3d5301d97a0f0fa22f
vladbuk <vladbuk@gmail.com>
2022-09-16 16:54:23 +0300

    07 func and args

---
+++ fortest/07_funcargs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+myfunc() {
+  for i in "$@"
+  do
+    echo $i
+  done
+}
+
+function myfunc2 {
+  for i in "$*"
+  do
+    echo $i
+  done
+}
+
+if [[ $# == 2 ]]
+then
+  myfunc $1 $2
+  myfunc2 $1 $2
+else
+  echo "We need 2 args"
+fi
